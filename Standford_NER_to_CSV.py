a7d742aca1c0664031ba012eb5d7aab7348da68d
River Smith <22485304+r-tae@users.noreply.github.com>
2022-04-08 16:32:37 +1000

    Update full_texts folder name reference

--- Standford_NER_to_CSV.py
+++ Standford_NER_to_CSV.py
@@ -27,11 +27,14 @@ from shapely.geometry import Point
 # As before make sure these link to your local versions of the libaries
 
 st = StanfordNERTagger(
-    os.path.normpath('./stanford-ner-2020-11-17/classifiers/english.all.3class.distsim.crf.ser.gz'),
-    os.path.normpath('./stanford-ner-2020-11-17/stanford-ner.jar'))
+    os.path.normpath(
+        "./stanford-ner-2020-11-17/classifiers/english.all.3class.distsim.crf.ser.gz"
+    ),
+    os.path.normpath("./stanford-ner-2020-11-17/stanford-ner.jar"),
+)
 
-# text_directory = os.path.normpath("texts/") 
-text_directory = os.path.normpath("short_texts/") 
+# text_directory = os.path.normpath("full_texts/")
+text_directory = os.path.normpath("short_texts/")
 csv_directory = os.path.normpath("ner_output/")
 
 if not os.path.exists(text_directory):
@@ -49,11 +52,11 @@ def build_url(placename: str, search_type: str, search_public_data: bool = True)
     """
     safe_placename = urllib.parse.quote(placename.strip().lower())
     url = f"https://tlcmap.org/ghap/search?"
-    if search_type == 'fuzzy':
+    if search_type == "fuzzy":
         url += f"fuzzyname={safe_placename}"
-    elif search_type == 'exact':
+    elif search_type == "exact":
         url = f"name={safe_placename}"
-    elif search_type == 'contains':
+    elif search_type == "contains":
         url = f"containsname={safe_placename}"
     else:
         return None
@@ -66,54 +69,61 @@ def build_url(placename: str, search_type: str, search_public_data: bool = True)
     url += "&format=json"
     return url
 
+
 def query_name(placename: str, search_type: str):
     """
     Use tlcmap/ghap API to check a placename, implemented fuzzy search but will not handle non returns.
     """
-    url = build_url(placename, search_type='fuzzy', search_public_data = False)
+    url = build_url(placename, search_type="fuzzy", search_public_data=False)
     if url:
         r = requests.get(url)
-        if r.url == 'https://tlcmap.org/ghap/maxpaging':
+        if r.url == "https://tlcmap.org/ghap/maxpaging":
             return None
-        if r.content.decode() == 'No search results to display.':
+        if r.content.decode() == "No search results to display.":
             # This should have obviously just be an empty list of features, but TLCMap is badly behaved
-            return json.loads('{"type": "FeatureCollection","metadata": {},"features": []}')
+            return json.loads(
+                '{"type": "FeatureCollection","metadata": {},"features": []}'
+            )
         if r.ok:
-            #data = json.loads(r.content)
+            # data = json.loads(r.content)
             return r.json()
     return None
 
+
 # ------- mapping stuff -------
 def map(filename, locations):
-    #df = pd.read_csv("Long_Lats.csv", delimiter=',', skiprows=0, low_memory=False)
+    # df = pd.read_csv("Long_Lats.csv", delimiter=',', skiprows=0, low_memory=False)
 
-    df = pd.DataFrame(locations, columns = ['placename', 'long', 'lat'])
-    geometry = [Point(xy) for xy in zip(df['long'], df['lat'])]
-    gdf = GeoDataFrame(df, geometry=geometry)   
+    df = pd.DataFrame(locations, columns=["placename", "long", "lat"])
+    geometry = [Point(xy) for xy in zip(df["long"], df["lat"])]
+    gdf = GeoDataFrame(df, geometry=geometry)
 
     # TODO: fit content to frame
-    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+    world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
 
     minx, miny, maxx, maxy = gdf.total_bounds
 
-    if not all(math.isinf(coord) for coord in [minx, miny, maxx, maxy]) \
-    and not all(math.isnan(coord) for coord in [minx, miny, maxx, maxy]):
+    if not all(math.isinf(coord) for coord in [minx, miny, maxx, maxy]) and not all(
+        math.isnan(coord) for coord in [minx, miny, maxx, maxy]
+    ):
         MAP_PADDING = 5
-        ax = world.plot(figsize=(10,6))
+        ax = world.plot(figsize=(10, 6))
         ax.set_xlim(minx - MAP_PADDING, maxx + MAP_PADDING)
         ax.set_ylim(miny - MAP_PADDING, maxy + MAP_PADDING)
 
-        gdf.plot(ax=ax, marker='o', color='red', markersize=15)
+        gdf.plot(ax=ax, marker="o", color="red", markersize=15)
 
-        plt.savefig(f"maps/{filename}.png", bbox_inches='tight')
-    
+        plt.savefig(f"maps/{filename}.png", bbox_inches="tight")
+
+
 # ------- end of mapping stuff -----
+
 
 def tlc_query(placename):
     response = query_name(placename, "fuzzy")
     # TODO: don't just choose the first result
-    if response and len(response['features']):
-        return response['features'][0]['geometry']['coordinates']
+    if response and len(response["features"]):
+        return response["features"][0]["geometry"]["coordinates"]
     else:
         return []
 
@@ -122,10 +132,8 @@ def textcheck(filename):
     start = time.time()
     print("Working on | ", filename)
     # set the specific path for the 'filename' which is basically working through a list of everything that is in the folder
-    textlocation = os.path.normpath(os.path.join(
-            text_directory,
-            filename))
-    text = open(textlocation, encoding='utf-8').read()
+    textlocation = os.path.normpath(os.path.join(text_directory, filename))
+    text = open(textlocation, encoding="utf-8").read()
 
     text_filename = os.path.basename(textlocation)
 
@@ -134,10 +142,10 @@ def textcheck(filename):
 
     locations = []
     entities = []  # this means entities, not locations
-    p_item = ('gibberish', '-1')  # ('New South Wales': 'LOCATION') (of, )
+    p_item = ("gibberish", "-1")  # ('New South Wales': 'LOCATION') (of, )
 
     for i in classified_text:
-        if p_item[1] != '-1' and i[1] != p_item[1] and p_item[1] != 'O':
+        if p_item[1] != "-1" and i[1] != p_item[1] and p_item[1] != "O":
             # Returning only named entities!
             if p_item[1] == "LOCATION":
                 results = tlc_query(p_item[0])
@@ -145,22 +153,25 @@ def textcheck(filename):
                 if len(location) == 3:
                     locations.append(location)
             entities.append(p_item)
-        if p_item[1] != 'O' and p_item[1] != '-1' and p_item[1] == i[1]:
-            p_item = (p_item[0] + ' ' + i[0], i[1])
+        if p_item[1] != "O" and p_item[1] != "-1" and p_item[1] == i[1]:
+            p_item = (p_item[0] + " " + i[0], i[1])
             continue
         p_item = i
-    if p_item[1] != 'O':
+    if p_item[1] != "O":
         entities.append(p_item)
 
     map(text_filename, locations)
-   
-    plusfile = [xs + (text_filename,) for xs in entities]  # this adds the FILEName into the CSV file
+
+    plusfile = [
+        xs + (text_filename,) for xs in entities
+    ]  # this adds the FILEName into the CSV file
 
     # print(locations)
 
-    with open(os.path.normpath(os.path.join(csv_directory, filename + ".csv")),
-              'w') as f:  # Writing these to a CSV file that has the same name as the text file
-        writer = csv.writer(f, delimiter=',', lineterminator='\n')
+    with open(
+        os.path.normpath(os.path.join(csv_directory, filename + ".csv")), "w"
+    ) as f:  # Writing these to a CSV file that has the same name as the text file
+        writer = csv.writer(f, delimiter=",", lineterminator="\n")
         writer.writerows(plusfile)
     end = time.time()
     print("Time elapsed | ", round(end - start, 2), "seconds")
